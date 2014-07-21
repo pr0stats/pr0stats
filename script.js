61aed7aa5cff24178aafe4334b1f7f14a894eefa
pr0stats developer <pr0stats@inbox.com>
2014-07-21 18:52:05 +0200

    Fix JS redirection path

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var target = 'http://pr0stats.github.io/';
 var url = document.URL.split('/');
-var query = url.splice(3, (url.length-1));
+var query = url.splice(4, (url.length-1));
 
 target = target + query.join('/');
 window.location.href = target;
